77358262d99b2e0a8310638e521005dab7c9ece2
Angel <angeldev.sawa@gmail.com>
2025-08-20 08:47:03 -0600

    mejora de disenio de not fond

--- src/app/shared/pages/not-found/not-found.component.ts
+++ src/app/shared/pages/not-found/not-found.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {IonContent} from "@ionic/angular/standalone";
+import {IonIcon} from "@ionic/angular/standalone";
+import {IonButton} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
   standalone: true,
-  imports: [IonContent] //si es estndalone true entonces puede importar elementos, ya que el componente es su propio modulo
+  imports: [IonContent, IonIcon, IonButton] //si es estndalone true entonces puede importar elementos, ya que el componente es su propio modulo
 })
 export class NotFoundComponent  implements OnInit {
 
