7841f2c047f41e9865d7eea68e2d63e88022b9f2
Angel <angeldev.sawa@gmail.com>
2025-08-21 07:57:18 -0600

    informacion dinamica

--- src/app/home/components/article/article.component.ts
+++ src/app/home/components/article/article.component.ts
@@ -8,7 +8,38 @@ import { Component, OnInit } from "@angular/core";
   //es un componente y no un modulo componente
 })
 export class ArticleComponent implements OnInit {
-  constructor() {}
+
+  //una variable de tipo ArticuloI
+  article: ArticuloI;
+
+  constructor() {
+    this.loadArticulo();
+  }
 
   ngOnInit() {}
+
+  loadArticulo(){
+      const data: ArticuloI  = {
+        title: 'Google empresa',
+        descripcion: 'google es una empresa estadounidense',
+        image: {
+          url: 'assets/g.png',
+          desc: 'no se que poner aqui'
+        },
+        id: '1'
+      };
+      this.article = data;
+  }
+}
+
+//interfaz de tipo articuloI
+interface ArticuloI{
+  title: string;
+  descripcion: string;
+  image: {
+    url: string;
+    desc: string;
+
+  }
+  id?: string;
 }
